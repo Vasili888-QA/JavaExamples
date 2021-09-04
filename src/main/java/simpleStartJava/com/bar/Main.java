cdd9d7f0c1265f026585e84167eb361d9bca2f4d
Vasili888-QA <vasil88888@inbox.ru>
2021-09-04 18:17:32 +0300

    Create class main and add Clients

--- src/main/java/simpleStartJava/com/bar/Main.java
+++ src/main/java/simpleStartJava/com/bar/Main.java
@@ -1,4 +1,13 @@
 package simpleStartJava.com.bar;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Client client1 = new Client("Vasili", "QA", 39, true);
+        Client client2 = new Client("Tatiana", "SV", 35, true);
+        Client client3 = new Client("Pavel", "Br", 20, true);
+        Client client4 = new Client("Alex", "QA", 39, false);
+
+    }
 }
