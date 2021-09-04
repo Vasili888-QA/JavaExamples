b79ebb2178d56215646add10e1024dd1559e5c96
Vasili888-QA <vasil88888@inbox.ru>
2021-09-04 18:42:23 +0300

    Create class Admin and add method checkClient

---
+++ src/main/java/simpleStartJava/com/bar/Admin.java
@@ -0,0 +1,14 @@
+package simpleStartJava.com.bar;
+
+public class Admin {
+
+    public void checkClient(String clientFirstName, String clientLastName, int clientAge, boolean clientIsID) {
+        if (clientIsID == false) {
+            System.out.println(clientFirstName + " " + clientLastName + " Sorry you don't have Driver License! Go home it's privet party");
+        } else if (clientAge < 20) {
+            System.out.println(clientFirstName + " " + clientLastName + " Sorry you so young! Go home" + clientFirstName);
+        } else {
+            System.out.println("Hello " + clientFirstName + " " + clientLastName + " Welcome to our privet party");
+        }
+    }
+}
